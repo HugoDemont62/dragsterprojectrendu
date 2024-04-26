9ef331632097b2ad4a523066e69a6a48b6fdc777
HugoDemont <hugodemont62@hotmail.fr>
2024-04-26 16:15:13 +0200

    init commit

--- base/models.py
+++ base/models.py
@@ -1,10 +1,24 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
 
 
+# Votre modèle UserProfile existant
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='userprofile')
     points = models.IntegerField(default=1000)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
 
 
 class Car(models.Model):
