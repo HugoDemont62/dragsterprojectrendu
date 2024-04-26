ac97601740f108b9f7fe8fec3d7b8d6266d64e16
HugoDemont <hugodemont62@hotmail.fr>
2024-04-26 11:34:55 +0200

    init commit

--- base/views.py
+++ base/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from .models import Car
+import random
 
 
 @login_required
 def home(request):
- return render(request, "home.html", {})
+    Car.objects.update_or_create(name='voiture-bleu', defaults={'max_speed': random.randint(100, 130)})
+    Car.objects.update_or_create(name='voiture-rouge', defaults={'max_speed': random.randint(100, 130)})
+    Car.objects.update_or_create(name='voiture-violette', defaults={'max_speed': random.randint(100, 130)})
+    Car.objects.update_or_create(name='voiture-orange', defaults={'max_speed': random.randint(100, 130)})
+
+    cars = Car.objects.all()
+
+    return render(request, 'home.html', {'cars': cars})
 
 
 def authView(request):
- if request.method == "POST":
-  form = UserCreationForm(request.POST or None)
-  if form.is_valid():
-   form.save()
-   return redirect("base:login")
- else:
-  form = UserCreationForm()
- return render(request, "registration/signup.html", {"form": form})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("base:login")
+    else:
+        form = UserCreationForm()
+    return render(request, "registration/signup.html", {"form": form})
